1091c71dc6c042b39e7a8ce00b6e1ab95e30bfc4
Musivo118 <gericgwapo9@gmail.com>
2025-02-26 05:08:09 +0800

    Dad

--- main.py
+++ main.py
@@ -95,7 +95,8 @@ async def generate_video(request: GenerateVideoRequest):
             "--driven_audio", audio_path,
             "--source_image", CACHED_IMAGE_PATH,
             "--result_dir", "/dev/shm",
-            "--preprocess", "full"
+            "--preprocess", "full",
+            "--enhancer",  "gfpgan" 
         ]
         logging.info(f"Running command: {' '.join(command)}")
         subprocess.run(command, check=True)
